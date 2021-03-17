ed98f2a6f6bc53dd6e56bf5c23082d345383cb75
SoleneMG <solene.michelgrosjean@gmail.com>
2021-03-17 15:23:15 +0100

    inject server

--- src/main/Inject.java
+++ src/main/Inject.java
@@ -1,4 +1,12 @@
 package main;
 
+import main.data.server.Server;
+import main.data.server.ServerImpl;
+
 public class Inject {
+    private static final Server server = new ServerImpl();
+
+    public static Server server(){
+        return server;
+    }
 }
